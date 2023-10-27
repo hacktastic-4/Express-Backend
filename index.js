c8a2680e4c13813ff0977694b7e12a12eda3e304
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-10-27 19:20:27 +0530

    Added all the data into postgres database

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const queryDatabase = require("./database/connect");
+const addDataToDB = require("./function");
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const app = express();
+const port = process.env.PORT || 3000;
+
+// const JudgeRouter = require("./routes/judge");
+// const TeamRouter = require("./routes/teams");
+// const ParameterRouter = require("./routes/parameters");
+// const ScoreRouter = require("./routes/score");
+// const CompetitionRouter = require("./routes/competition");
+// const ResultRouter = require("./routes/result");
+
+// const { notFound } = require("./middleware/notFound");
+
+app.use(express.json());
+
+// Routes
+// app.use("/api/v1/judge", JudgeRouter);
+// app.use("/api/v1/team", TeamRouter);
+// app.use("/api/v1/parameter", ParameterRouter);
+// app.use("/api/v1/score", ScoreRouter);
+// app.use("/api/v1/competition", CompetitionRouter);
+// app.use("/api/v1/result", ResultRouter);
+
+// app.use(notFound);
+
+const start = async () => {
+  try {
+    await queryDatabase("SELECT 1"); // A simple query to test the connection
+    console.log("Database connected successfully");
+
+    // await addDataToDB();
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
